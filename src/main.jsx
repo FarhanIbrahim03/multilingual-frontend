a0dd2848b912ef726f736c69b99ef1a542b673c5
Farhanibrahim03 <mifarhan03@gmail.com>
2025-03-20 15:29:07 +0530

    Updated main.jsx with latest changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ParallaxProvider } from "react-scroll-parallax";
 import App from "./App.jsx";
 import "./index.css";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
